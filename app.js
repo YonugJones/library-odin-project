913704ac7a5f60f67513d81418c883a922f05fc8
YonugJones <peterckerfoot@gmail.com>
2023-11-05 09:57:33 -0600

    Made buttons on new book form work.

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 const myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
 }
 
 function addBookToLibrary() {
-  // do stuff here
+
 }
+
+let newBookBtn = document.getElementById('new-book-btn');
+newBookBtn.addEventListener('click', function() {
+    let newBookForm = document.getElementById('new-book-form');
+    newBookForm.style.display = 'flex';
+});
+
+document.getElementById('submit-btn').addEventListener('click', function(e) {
+    e.preventDefault();
+    addBookToLibrary();
+});
+
+document.getElementById('cancel-btn').addEventListener('click', function(e) {
+    e.preventDefault();
+    let newBookForm = document.getElementById('new-book-form');
+    newBookForm.style.display = 'none';
+});
