80ba85f280002a5e9981136011bf0c99798b1c4b
YonugJones <peterckerfoot@gmail.com>
2023-11-05 10:58:03 -0600

    Added functin that displays myLibrary on page

--- app.js
+++ app.js
@@ -8,8 +8,62 @@ function Book(title, author, pages, read) {
 }
 
 function addBookToLibrary() {
+    let title = document.getElementById('title').value;
+    let author = document.getElementById('author').value;
+    let pagesInput = document.getElementById('pages').value;
+    let pages = parseInt(pagesInput);
+    let read = document.getElementById('read').value;
 
-}
+    if (isNaN(pages) || pages <= 0) {
+        alert('Please enter a valid number')
+        return;
+    }
+
+    let newBookForm = document.getElementById('new-book-form');
+    newBookForm.style.display = 'none';
+
+    let newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+    updateDisplay();
+};
+
+function updateDisplay() {
+    let bookShelf = document.getElementById('book-shelf');
+
+    while(bookShelf.firstChild) {
+        bookShelf.removeChild(bookShelf.firstChild);
+    }
+
+    for (let i = 0; i < myLibrary.length; i++) {
+        let book = myLibrary[i];
+
+        let bookCard = document.createElement('div');
+        bookCard.classList.add('book-card');
+
+        let cardTitle = document.createElement('h2');
+        cardTitle.classList.add('card-title');
+        cardTitle.textContent = `${book.title}`;
+
+        let cardAuthor = document.createElement('p');
+        cardAuthor.classList.add('card-author');
+        cardAuthor.textContent = `${book.author}`;
+
+        let cardPages = document.createElement('p');
+        cardPages.classList.add('p');
+        cardPages.textContent = `${book.pages} pages`;
+
+        let cardRead = document.createElement('p');
+        cardRead.classList.add('card-read');
+        cardRead.textContent = `${book.read ? 'Completed' : 'Not yet completed'}`;
+
+        bookCard.appendChild(cardTitle);
+        bookCard.appendChild(cardAuthor);
+        bookCard.appendChild(cardPages);
+        bookCard.appendChild(cardRead);
+
+        bookShelf.appendChild(bookCard);
+    }
+};
 
 let newBookBtn = document.getElementById('new-book-btn');
 newBookBtn.addEventListener('click', function() {
